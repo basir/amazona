7d86fd3398d50f84be8acde980cbb039c4c5df22
Basir <basir.jafarzadeh@gmail.com>
2020-09-21 21:16:56 +0330

    Video-07-Create-Rating-And-Product-Component

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Rating from './Rating';
+
+export default function Product(props) {
+  const { product } = props;
+  return (
+    <div key={product._id} className="card">
+      <a href={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt={product.name} />
+      </a>
+      <div className="card-body">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <div className="price">${product.price}</div>
+      </div>
+    </div>
+  );
+}
