8cb98e7e872ded2248ca423f0a2bb8436c59a9a4
Basir <basir.jafarzadeh@gmail.com>
2020-09-21 15:57:42 +0330

    Video-05-Create-React-App

--- README.md
+++ README.md
@@ -20,3 +20,10 @@
    1. create products div
    2. add product attributes
    3. add link, image, name and price
+5. Create React App
+   1. npx create-react-app frontend
+   2. npm start
+   3. Remove unused files
+   4. copy index.html content to App.js
+   5. copy style.css content to index.css
+   6. replace class with className
---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import data from './data';
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <div key={product._id} className="card">
+                <a href={`/product/${product._id}`}>
+                  <img
+                    className="medium"
+                    src={product.image}
+                    alt={product.name}
+                  />
+                </a>
+                <div className="card-body">
+                  <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                  </a>
+                  <div className="rating">
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                    <span>
+                      <i className="fa fa-star"></i>
+                    </span>
+                  </div>
+                  <div className="price">${product.price}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
