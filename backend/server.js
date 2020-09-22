c5a30e1d68128b4b01b4ded820c47fd50c06c295
Basir <basir.jafarzadeh@gmail.com>
2020-09-22 19:27:02 +0330

    Video-08-Create-NodeJS-Server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
