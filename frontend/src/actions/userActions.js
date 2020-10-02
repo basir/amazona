8342192200244391924c382e99524aacaf641787
Basir <basir.jafarzadeh@gmail.com>
2020-10-02 19:16:37 +0330

    Video-22-Implement-SignIn-Action

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+import {
+  USER_SIGNIN_FAIL,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNOUT,
+} from '../constants/userConstants';
+
+export const signin = (email, password) => async (dispatch) => {
+  dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
+  try {
+    const { data } = await Axios.post('/api/users/signin', { email, password });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_SIGNIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+export const signout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  localStorage.removeItem('cartItems');
+  dispatch({ type: USER_SIGNOUT });
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,8 +5,14 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
+import { userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
+  userSignin: {
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -17,6 +23,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userSignin: userSigninReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
