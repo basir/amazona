843920d66835f4b177d5e0b910347a371da50b8d
Basir <basir.jafarzadeh@gmail.com>
2020-10-04 16:09:27 +0330

    Video-27-Create-Place-Order-API

--- README.md
+++ README.md
@@ -168,3 +168,8 @@
 26. Design Place Order Screen
     1. design order summary fields
     2. design order action
+27. Create Place Order API
+    1. createOrder api
+    2. create orderModel
+    3. create orderRouter
+    4. create post order route
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
