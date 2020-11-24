a4b82acd9caac702b856b1b3e77cd3a889272d2a
Basir <basir.jafarzadeh@gmail.com>
2020-11-24 11:24:39 +0330

    Video-52-Force-Order-Items-From-One-Seller

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,5 +1,6 @@
 import {
   CART_ADD_ITEM,
+  CART_ADD_ITEM_FAIL,
   CART_EMPTY,
   CART_REMOVE_ITEM,
   CART_SAVE_PAYMENT_METHOD,
@@ -14,24 +15,28 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       if (existItem) {
         return {
           ...state,
+          error: '',
           cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
       } else {
-        return { ...state, cartItems: [...state.cartItems, item] };
+        return { ...state, error: '', cartItems: [...state.cartItems, item] };
       }
     case CART_REMOVE_ITEM:
       return {
         ...state,
+        error: '',
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
       };
     case CART_SAVE_SHIPPING_ADDRESS:
       return { ...state, shippingAddress: action.payload };
     case CART_SAVE_PAYMENT_METHOD:
       return { ...state, paymentMethod: action.payload };
+    case CART_ADD_ITEM_FAIL:
+      return { ...state, error: action.payload };
     case CART_EMPTY:
-      return { ...state, cartItems: [] };
+      return { ...state, error: '', cartItems: [] };
     default:
       return state;
   }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,7 +10,7 @@ export default function CartScreen(props) {
     ? Number(props.location.search.split('=')[1])
     : 1;
   const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
+  const { cartItems, error } = cart;
   const dispatch = useDispatch();
   useEffect(() => {
     if (productId) {
@@ -30,6 +30,7 @@ export default function CartScreen(props) {
     <div className="row top">
       <div className="col-2">
         <h1>Shopping Cart</h1>
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         {cartItems.length === 0 ? (
           <MessageBox>
             Cart is empty. <Link to="/">Go Shopping</Link>
