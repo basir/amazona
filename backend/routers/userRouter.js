c651e174b29fa1564795489eb3863acd735417da
Basir <basir.jafarzadeh@gmail.com>
2020-12-11 09:49:21 +0330

    Video-58-BugFix-Running-Locally-Without-Issue

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -146,8 +146,9 @@ userRouter.put(
     if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
-      user.isSeller = req.body.isSeller || user.isSeller;
-      user.isAdmin = req.body.isAdmin || user.isAdmin;
+      user.isSeller = Boolean(req.body.isSeller);
+      user.isAdmin = Boolean(req.body.isAdmin);
+      // user.isAdmin = req.body.isAdmin || user.isAdmin;
       const updatedUser = await user.save();
       res.send({ message: 'User Updated', user: updatedUser });
     } else {
