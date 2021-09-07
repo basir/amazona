2aeb6892c14f0c22d6a6b5686ed00227d482d128
Marcelo <marcelo95326@icloud.com>
2021-09-07 16:36:14 -0400

    part13

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,9 @@ const {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } = require('../constants/productConstants');
 
 export const productListReducer = (
@@ -19,3 +22,20 @@ export const productListReducer = (
       return state;
   }
 };
+
+export const productDetailsReducer = (
+  state = { product: {}, loading: true },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+  
